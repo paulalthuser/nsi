2019204d55fdc46db3d08bef2eb7af233f65416b
paulalthuser <130064880+paulalthuser@users.noreply.github.com>
2023-04-06 15:06:23 +0200

    Delete Projret NHeSsI directory

--- Projret NHeSsI/JS/page1.js
+++
@@ -1,9 +0,0 @@
-function findOS() {
-    var OS = "inconnu"
-    if (navigator.userAgent.indexOf("Mac") != -1) OS =  "Macintosh"; 
-    if (navigator.userAgent.indexOf("Linux") != -1) OS =  "Linux"; 
-    if (navigator.userAgent.indexOf("Android") != -1) OS =  "Android"; 
-    if (navigator.userAgent.indexOf("like Mac") != -1) OS =  "iOS";
-    if (navigator.userAgent.indexOf("Win") != -1) OS = "Windows"; 
-    document.getElementById("OS").innerHTML = OS;
-}
